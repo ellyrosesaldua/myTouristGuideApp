976989773201ab0688a7e537ab9c6c7a0443396e
ellyrosesaldua <ellyrosesaldua165@gmail.com>
2016-07-12 00:40:08 +0200

    myTouristGuideApp

---
+++ myTouristGuideApp.git/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('cartCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+      
+.controller('page4Ctrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+   
+.controller('page2Ctrl', function($scope) {
+
+})
+ 
